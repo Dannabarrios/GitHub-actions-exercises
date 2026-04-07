2c242a2795be892989a71033529571faf11bee4f
Dannabarrios <dannabarrios0820@gmail.com>
2026-04-06 22:05:13 -0500

    update

--- ejercicio-2-javascript/index.js
+++ ejercicio-2-javascript/index.js
@@ -1,10 +1,4 @@
  
-const core = require('@actions/core');
-
-try {
-  const nombre = core.getInput('nombre') || 'Mundo';
-  console.log(`Hola ${nombre} desde una accion JavaScript!`);
-  core.setOutput('mensaje', `Hola ${nombre}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const nombre = process.env.INPUT_NOMBRE || 'Mundo';
+console.log(`Hola ${nombre} desde una accion JavaScript!`);
+console.log(`::set-output name=mensaje::Hola ${nombre}`);
